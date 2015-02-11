f00f75dbf73b23a932f12f280ac514cad0220560
Dirk Boller <dirkboller@Christines-Mini.fritz.box>
2015-02-11 14:35:05 +0100

    Initial Commit

---
+++ ShoeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeConverter
+//
+//  Created by Dirk Boller on 11.02.15.
+//  Copyright (c) 2015 Dirk Boller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
